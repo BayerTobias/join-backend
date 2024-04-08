900c31166de5638923dd256073e0e5bc1bf8e49c
BayerTobias <tbayer2@gmx.de>
2024-04-08 19:41:44 +0200

    serializers commented

--- join_backend/serializers.py
+++ join_backend/serializers.py
@@ -2,12 +2,22 @@ from rest_framework import serializers
 from join.models import Task, Category, CustomUser, Contact
 
 
+"""
+Serializer for Task model.
+"""
+
+
 class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
         fields = "__all__"
         read_only_fields = ["author", "assigned_users"]
+
+
+"""
+ Serializer for Category model.
+"""
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -17,11 +27,21 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+"""
+Serializer for list view of CustomUser model.
+"""
+
+
 class UserListSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CustomUser
         fields = ["id", "initials", "color", "first_name", "last_name"]
+
+
+"""
+Serializer for detail view of CustomUser model.
+"""
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -37,6 +57,11 @@ class UserSerializer(serializers.ModelSerializer):
             "last_name",
             "email",
         ]
+
+
+"""
+Serializer for Contact model.
+"""
 
 
 class ContactSerializer(serializers.ModelSerializer):
